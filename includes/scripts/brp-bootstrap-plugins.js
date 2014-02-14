1b60c2af8542cae7afd3cfe6743477a2031f081e
Derek Misler <derekmisler@gmail.com>
2014-02-14 17:07:26 -0500

    getting the scrolling to recognize the slide number

--- includes/scripts/brp-bootstrap-plugins.js
+++ includes/scripts/brp-bootstrap-plugins.js
@@ -128,7 +128,7 @@ $(document).ready(function(){
 
     //waypoints doesnt detect the first slide when user scrolls back up to the top so we add this little bit of code, that removes the class from navigation link slide 2 and adds it to navigation link slide 1. 
     mywindow.scroll(function () {
-        if (mywindow.scrollTop() == 0) {
+        if (mywindow.scrollTop() <= 1) {
             $('.navigation li[data-slide="1"]').addClass('currentslide');
             $('.navigation li[data-slide="2"]').removeClass('currentslide');
         }
@@ -136,20 +136,11 @@ $(document).ready(function(){
 
     //Create a function that will be passed a slide number and then will scroll to that slide using jquery easing.
     function goToByScroll(dataslide) {
-        htmlbody.animate({
-            scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
-        }, 1000, 'easeInOutQuint');
+        htmlbody.animate({ scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top + 1 }, 2000, 'easeInOutQuint');
     }
 
     //When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
     links.click(function (e) {
-        e.preventDefault();
-        dataslide = $(this).attr('data-slide');
-        goToByScroll(dataslide);
-    });
-
-    //When the user clicks on the button, get the data-slide attribute value of the button and pass that variable to the goToByScroll function
-    button.click(function (e) {
         e.preventDefault();
         dataslide = $(this).attr('data-slide');
         goToByScroll(dataslide);
